314f3b4dc3566bb03a17c357595b9404a8686bb1
Drake Hager <Drake_hager@insideranken.org>
2023-12-19 09:07:27 -0600

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/final-project",
+  base: "/.github.io-react-project",
   plugins: [react()],
 })
