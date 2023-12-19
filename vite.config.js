a9a2a3ba6eb430d5f4689a367224020f81eead4a
Drake Hager <Drake_hager@insideranken.org>
2023-12-19 09:24:50 -0600

    pretty please work

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/.github.io-react-project",
+  base: "/npm react-project",
   plugins: [react()],
 })
